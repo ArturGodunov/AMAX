0841b76550487dc9222e0dfa5b10ad43b731dd49
ArturGodunov <artur-godunov@mail.ru>
2016-07-25 21:00:57 +0300

    add header

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -2,14 +2,31 @@ var app = (function ($) {
     "use strict";
 
     /** Private properties */
-    //var properties,
-        /** Private methods */
-        //methods;
+
+    /** Private methods */
+
 
     return {
+        tabs: function () {
+            $('[data-tab-link]').on('click', function () {
+                var index = $(this).data('tab-link');
+
+                $(this)
+                    .addClass('active')
+                    .siblings()
+                    .removeClass('active');
+
+                $(this)
+                    .parents('[data-tab-wrap]')
+                    .find('[data-tab-body="' + index + '"]')
+                    .addClass('active')
+                    .siblings()
+                    .removeClass('active');
+            });
+        },
 
         init: function() {
-
+            app.tabs();
         }
 
     };
